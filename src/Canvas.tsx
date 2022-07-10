6be715365b7be62e2633752512fcf1c89a8d5755
Miyaboom0825 <miyaboom0825@gmail.com>
2022-07-10 22:01:38 +0900

    ノイズでマーブル模様を作る

--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -2,8 +2,10 @@ import { useEffect, useRef } from "react";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
-import vertShader from './shaders/sphere.vert'
-import fragShader from './shaders/sphere.frag'
+import vertShader from "./shaders/sphere.vert";
+import fragShader from "./shaders/sphere.frag";
+import skyVertShader from "./shaders/sky.vert";
+import skyFragShader from "./shaders/sky.frag";
 
 export default function Canvas() {
   const ref = useRef<HTMLDivElement>(null);
@@ -31,35 +33,67 @@ export default function Canvas() {
     const camera = new THREE.PerspectiveCamera(45, width / hight, 1, 10000);
     // カメラコントローラーを作成
     const controls = new OrbitControls(camera, renderer.domElement);
-    camera.position.set(0, 0, 50);
+    camera.position.set(0, 0, 75);
     controls.update();
 
     // カメラ移動に慣性を持たせる
     controls.enableDamping = true;
     controls.dampingFactor = 0.5;
 
+    // 平行光源を作成
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.21);
+    directionalLight.position.set(10, 10, 10);
+    scene.add(directionalLight);
+
+    // 環境光源を作成
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+    scene.add(ambientLight);
+
     // 球を作成
-    let time = 0.0
-    const uniforms = {
-      time: { value: time }
-    };
+    let time = 0.0;
 
     const geometry = new THREE.SphereGeometry(15, 32, 16);
     const material = new THREE.ShaderMaterial({
-      uniforms: uniforms,
+      uniforms: THREE.UniformsUtils.merge([
+        THREE.UniformsLib.lights,
+        {
+          time: { value: time },
+          diffuse: { value: new THREE.Color(0xffffff) },
+          emissive: { value: new THREE.Color(0x000000) },
+          color1: { value: new THREE.Color(0xffffff) },
+          color2: { value: new THREE.Color(0xcccccc) },
+        }
+      ]),
+      lights: true,
       vertexShader: vertShader,
       fragmentShader: fragShader,
-      side: THREE.BackSide
     });
 
     const sphere = new THREE.Mesh(geometry, material);
     scene.add(sphere);
 
+    // 天球を作成
+    const skyGeo = new THREE.SphereGeometry(4000, 32, 15);
+    const skyMat = new THREE.ShaderMaterial({
+      uniforms: {
+        topColor: { value: new THREE.Color(0xcdcdcd) },
+        bottomColor: { value: new THREE.Color(0xffffff) },
+        offset: { value: 0 },
+        exponent: { value: 0.6 },
+      },
+      vertexShader: skyVertShader,
+      fragmentShader: skyFragShader,
+      side: THREE.BackSide,
+    });
+
+    const sky = new THREE.Mesh(skyGeo, skyMat);
+    scene.add(sky);
+
     // アニメーション
     function tick() {
-      if(time === 1.0) time = 0.0
-      sphere.material.uniforms.time.value = time
-      time += 0.01
+      if (time === 1.0) time = 0.0;
+      sphere.material.uniforms.time.value = time;
+      time += 0.1;
 
       // カメラコントローラーを更新
       controls.update();
@@ -71,7 +105,7 @@ export default function Canvas() {
     }
 
     // 初回実行
-    tick();
+    const animationId = requestAnimationFrame(tick);
 
     function onResize() {
       // サイズを取得
@@ -94,6 +128,7 @@ export default function Canvas() {
     onResize();
 
     return () => {
+      cancelAnimationFrame(animationId);
       window.removeEventListener("resize", onResize);
       elm?.removeChild(renderer.domElement);
     };
--- src/shaders/sphere.vert
+++ src/shaders/sphere.vert
@@ -1,8 +1,15 @@
+varying vec3 vViewPosition;
+varying vec3 vNormal;
 varying vec3 vWorldPosition;
 
 void main() {
+    vNormal = normalMatrix * normal;
+    
     vec4 worldPosition = modelMatrix * vec4(position, 1.0);
     vWorldPosition = worldPosition.xyz;
-
+    
+    vec4 mvPosition = viewMatrix * worldPosition;
+    vViewPosition = mvPosition.xyz;
+    
     gl_Position = projectionMatrix * viewMatrix * worldPosition;
 }
